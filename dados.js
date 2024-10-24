52892bb4126701a5561f8bc06a66ffe8243123f4
leneribeiro <rosileneolegario24@gmail.com>
2024-10-24 13:07:06 -0300

    minha versão da imersão DEV do Google

---
+++ dados.js
@@ -0,0 +1,45 @@
+let dados = [
+    {
+        titulo: "Dulce Maria",
+        descricao: "Dulce Maria,foi uma das integrantes mais queridas do grupo mexicano RBD, que ganhou fama mundial com a novela Rebelde em 2004 atuando como Roberta. Além de sua carreira musical com o RBD, Dulce Maria também lançou um disco solo chamado Extranjera em 2010, que incluiu o sucesso Inevitable Ela é conhecida por sua voz cativante e presença de palco carismática, além de ter uma carreira de atriz que inclui participações em telenovelas e filmes. Ela também tem uma conexão famosa, sendo sobrinha-neta da icônica pintora Frida Kahlo,Dulce Maria continua a encantar fãs ao redor do mundo com sua música e atuação, e recentemente fez uma turnê chamada Soy Rebelde Tour junto com os outros membros do RBD, que foi um grande sucesso.",
+        link: "https://pt.wikipedia.org/wiki/Dulce_Mar%C3%ADa",
+        tags: "roberta Roberta dulce maria roberta pardo ROoberta Pardo "
+    },
+    {
+        titulo: "Anahi",
+        descricao: " Anahí foi uma das integrantes mais populares do grupo mexicano RBD, que ganhou fama mundial com a novela Rebelde em 2004 atuando como Mia. Além de sua carreira musical com o RBD, Anahí lançou dois álbuns solo: Baby Blue em 2000 e Mi Delirio em 2009, que incluiu o sucesso Mi Delirio. Ela é conhecida por sua voz poderosa e presença de palco carismática. Anahí também teve uma carreira de atriz, participando de várias telenovelas e filmes. Após a separação do RBD, ela continuou sua carreira solo e também se aventurou em produções teatrais. Anahí é uma das artistas mais queridas pelos fãs do RBD e continua a ser uma figura importante na música latina.",
+        link: "https://pt.wikipedia.org/wiki/Anah%C3%AD",
+        tags: "mia Mia MIA Mia colucci Anahi ANAHI"
+    },
+    {
+        titulo: "Maite Perroni",
+        descricao: "Maite Perroni foi uma das integrantes mais queridas do grupo mexicano RBD, que ganhou fama mundial com a novela Rebelde em 2004 atuando como Lupita . Além de sua carreira musical com o RBD, Maite lançou dois álbuns solo: Eclipse de Luna em 2013 e Deseo em 2016. Ela é conhecida por sua voz encantadora e presença de palco cativante. Maite também teve uma carreira de atriz, participando de várias telenovelas e filmes, como Cuidado con el ángel e La Mujer del Vendaval. Após a separação do RBD, ela continuou sua carreira solo e também se aventurou em produções teatrais. Maite é uma das artistas mais queridas pelos fãs do RBD e continua a ser uma figura importante na música latina.",
+        link: "https://pt.wikipedia.org/wiki/Maite_Perroni",
+        tags: " LUPITA Lupita Maite MAITE PERRONI maite "
+    },
+
+
+    {
+        titulo: "Christian Chávez",
+        descricao: "Christian Chávez foi um dos integrantes mais queridos do grupo mexicano RBD, que ganhou fama mundial com a novela Rebelde em 2004 atuando como Geovani. Além de sua carreira musical com o RBD, Christian lançou um álbum solo chamado Almas Gemelas em 2010. Ele é conhecido por sua voz poderosa e presença de palco carismática. Christian também é conhecido por ter se assumido gay em 2007, após fotos de seu casamento no Canadá vazarem. Desde então, ele tem sido um ativista dos direitos LGBTQIA+ e continua a ser uma figura importante na música e na comunidade",
+        link: "https://pt.wikipedia.org/wiki/Christian_Ch%C3%A1vez",
+        tags: " geovani GEOVANI Geovani cris CHRISTIAN CHAVES cristian chaves "
+    },
+
+    {
+        titulo: "Christopher",
+        descricao: "Christopher von Uckermann, conhecido simplesmente como Christopher, foi um dos integrantes mais queridos do grupo mexicano RBD, que ganhou fama mundial com a novela Rebelde em 2004 atuando como Diego. Além de sua carreira musical com o RBD, Christopher lançou um álbum solo chamado Libertad em 2010. Ele é conhecido por sua voz poderosa e presença de palco carismática. Christopher também teve uma carreira de ator, participando de várias telenovelas e filmes, como Rebelde e Verano de Amor. Após a separação do RBD, ele continuou sua carreira solo e também se aventurou em produções teatrais. Christopher é uma das figuras mais queridas pelos fãs do RBD e continua a ser uma figura importante na música latina. ",
+        link: "https://pt.wikipedia.org/wiki/Christopher_Uckermann ",
+        tags: " diego Diego Bustamante Christopher Uckermann CHRISTOPHER UCKERMANN  "
+    },
+
+    {
+        titulo: "Alfonso Herrera",
+        descricao: " Alfonso Herrera foi um dos integrantes mais queridos do grupo mexicano RBD, que ganhou fama mundial com a novela Rebelde em 2004 atuando como Miguel. Além de sua carreira musical com o RBD, Alfonso lançou um álbum solo chamado Hoy No Me Puedo Levantar em 2010. Ele é conhecido por sua voz poderosa e presença de palco carismática. Alfonso também teve uma carreira de ator, participando de várias telenovelas e filmes, como Clase 406 e Venezzia. Após a separação do RBD, ele continuou sua carreira solo e também se aventurou em produções teatrais. Alfonso é uma das figuras mais queridas pelos fãs do RBD e continua a ser uma figura importante na música latina.",
+        link: " https://pt.wikipedia.org/wiki/Alfonso_Herrera ",
+        tags: " MIGUEL miguel Miguel Alfonso alfonso herrera "
+
+    }
+
+
+];
